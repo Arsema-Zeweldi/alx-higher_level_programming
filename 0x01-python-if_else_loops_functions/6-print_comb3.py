1a35ef19200c3a23baa92247f26f22ce51a0d7ab
Arsema-Zeweldi <arsugeb@gmail.com>
2022-05-31 07:46:47 -0700

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for a in range(10):
-    for b in range (a + 1, 10):
-        print("{}{}".format(a, b), end=", " if int(repr(a) + repr(b)) < 89 else "\n")
+    for b in range(a + 1, 10):
+        print("{}{}".format(a, b), end=", " 
+                if int(repr(a) + repr(b)) < 89 else "\n")
