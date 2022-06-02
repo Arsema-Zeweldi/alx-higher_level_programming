aae204b0bb62d52f2304cf2a533383e944264506
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-02 08:07:33 -0700

    3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(argv):
+    a = len(argv) - 1
+    if a == 0:
+        print("{:d}".format(a))
+        return
+    else:
+        b = 1
+        c = 0
+        while b <= a:
+            c += int(argv[b])
+            b += 1
+        print("{:d}".format(c))
+
+if __name__ == "__main__":
+    import sys
+    add(sys.argv)
