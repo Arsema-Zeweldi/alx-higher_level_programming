cbce648cfae1d20720188193371ec353f04d5b45
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-02 03:49:14 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 1:
-    print("{:d} arguments.".format(len(argv) - 1))
-elif len(argv) == 2:
-    print("{:d} arguments:".format(len(argv) - 1))
-else:
-    print("{:d} arguments:".format(len(argv) - 1))
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, str(argv[i])))
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{:d} arguments:".format(len(argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, str(argv[i])))
