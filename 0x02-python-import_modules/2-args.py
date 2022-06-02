db713b6285ae7f1f7709919b99ae2ff5e3b57f2c
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-02 03:42:15 -0700

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
+elif len(argv) == 2:
+    print("{:d} arguments:".format(len(argv) - 1))
+else:
+    print("{:d} arguments:".format(len(argv) - 1))
+for i in range(1, len(argv)):
+    print("{:d}: {:s}".format(i, str(argv[i])))
