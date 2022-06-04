572f0db59ed18797c0f2303f0b4b20f39f59b47f
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-03 23:22:23 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if len(sentence) != 0:
+        tuple = (len(sentence), sentence[0])
+        return tuple
+    else:
         return None
-    tuple = (len(sentence), sentence[0])
-    return tuple
