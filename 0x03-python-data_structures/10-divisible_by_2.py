e96a34d7035f83aa75303ad668f95492790df542
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-03 23:47:50 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return False
-    else:
-        for n in my_list:
-            if n % 2 == 0:
-                return n
+    if not my_list:
+        return
+    for n in my_list:
+        if n % 2 == 0:
+            return n
