55e2b976ba77f11cb3029df3dfb2b58359d3a8ef
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-08 12:26:21 -0700

    6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
         for k, v in sorted(a_dictionary.items()):
-            print("{} : {}".format(k, v))
+            print("{}: {}".format(k, v))
