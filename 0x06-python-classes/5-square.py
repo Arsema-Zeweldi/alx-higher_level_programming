e158448c128276cfb00d733f38067cd09de28560
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-21 13:18:40 -0700

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" The Square class defines square"""
+
+
+class Square:
+    "Private instance attribut - size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (int(self.__size) ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for n in range(self.__size):
+            print("{}".format('#' * self.__size))
