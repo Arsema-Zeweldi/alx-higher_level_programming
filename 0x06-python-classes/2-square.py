81aeb6c11f3a0f19484f051ff7dd32bd763e9f58
Arsema-Zeweldi <arsugeb@gmail.com>
2022-06-21 12:01:25 -0700

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """Private instance attribute"""
     def __init__(self, size=0):
         if type(size) != int:
-            raise ValueError("size must be >= 0")
-        elif size < 0:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
