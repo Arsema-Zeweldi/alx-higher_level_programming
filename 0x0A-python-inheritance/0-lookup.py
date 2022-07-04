4a376c51dea6562e3f965094b4c97199c4b76820
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-04 05:35:51 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Returns all the attributes and methods available in a class"""
+
+
+def lookup(obj):
+    """All the attributes an methods in class obj"""
+    return dir(obj)
