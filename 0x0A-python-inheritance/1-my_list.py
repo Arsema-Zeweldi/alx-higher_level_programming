269374f27587810a1ae0e02d7e63009ec45c3e41
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-04 09:06:34 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" The MyList class inherits list"""
+
+
 class MyList(list):
+    """public instance method"""
     def print_sorted(self):
+        """sorted list"""
         new_list = (sorted(self))
-        print (new_list)
+        print(new_list)
