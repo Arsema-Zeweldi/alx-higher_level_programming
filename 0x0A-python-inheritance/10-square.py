bccfebb6d1ed3c884f34425b923a21c56d268506
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-04 12:55:00 -0700

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
