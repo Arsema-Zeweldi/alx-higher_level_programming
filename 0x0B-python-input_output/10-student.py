d92edf02f906087376f9a8f669c150037f73d0fe
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-05 09:38:12 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a student"""
+
+
+class Student:
+    """The Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Public instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json to dict"""
+        try:
+            for attr in attrs:
+                if type(attr) != str:
+                    return attrs.__dict__
+        except Exception:
+            return self.__dict__
+        new_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
