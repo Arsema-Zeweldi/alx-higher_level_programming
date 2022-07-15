eff3c8160141c5295ce0ef5f778ed9819d53e39a
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-15 05:12:19 -0700

    test_sq

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -9,13 +9,16 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
     """cases to be evaluated"""
     def test_init(self):
+        """Tests automatic assignment of id"""
         s1 = Square(5)
+        self.assertEqual(s1.id, 6)
+        """Tests automatic assignment of id+1"""
         s2 = Square(10)
-        self.assertEqual(s1.id, 1)
-        self.assertEqual(s2.id, 2)
+        self.assertEqual(s2.id, 7)
 
     def test_init_2(self):
         self.assertRaises(TypeError, Square)
+
 
 if __name__ == '__main__':
     unittest.main()
