cd4fcb3ca2eb24035fd5ad35ccaac39b3d0d4f2e
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-11 06:34:46 -0700

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- lists all shows contained in 
 -- hbtn_0d_tvshows that have 
 -- at least one genre linked
-SELECT tv_shows.title, tv_shows_genres.genre_id
-	FROM tv_show_genres
-	JOIN tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_shows
+	INNER JOIN tv_show_genres
 	ON tv_show_genres.show_id = tv_shows.id
 	ORDER BY tv_shows.title, tv_show_genres.genre_id;
