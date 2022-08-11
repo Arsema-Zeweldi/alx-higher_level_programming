df44f277eacc784c0e8c6cd05a5a9b25de570082
Arsema-Zeweldi <arsugeb@gmail.com>
2022-08-11 06:45:29 -0700

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- in the database hbtn_0d_usa
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
-	FROM states
-	INNER JOIN cities
+	FROM cities
+	INNER JOIN states
 	ON states.id  = cities.state_id
 	ORDER BY cities.id ASC;
