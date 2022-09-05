f55fce7a59606f03ecbbe78ec584ac6dcca03f51
Arsema-Zeweldi <arsugeb@gmail.com>
2022-09-05 15:43:00 +0000

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log('C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing');
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
