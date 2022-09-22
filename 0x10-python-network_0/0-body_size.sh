0ddeae4939d02e03cf38306edfcb8007321081f4
Arsema-Zeweldi <arsugeb@gmail.com>
2022-09-22 13:06:09 -0700

    fix error 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#takes a URL, sends a request to that URL, and displays the size of the
-#body of the response
+#takes a URL, sends a request to that URL, and displays the size of the body
 curl -sI "$1" | grep Content-Length | cut -d " " -f2
